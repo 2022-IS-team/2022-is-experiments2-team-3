fc1f822b0a9f7183e2fff16747081efae517fbdf
MoAI522 <urotanre522@gmail.com>
2022-11-04 09:35:06 +0000

    state_to_observation実装指示

--- aurl/environment/controller/state_to_observation.py
+++ aurl/environment/controller/state_to_observation.py
@@ -1,2 +1,16 @@
-def state_to_observation():
+from ..model import GameState
+from gym import spaces
+
+
+def state_to_observation(state: GameState) -> spaces.Space:
+    """state_to_observation
+
+    GameStateをobservation_spaceの表現に変換する。
+
+    Args:
+        state (GameState): 現状のゲーム情報
+
+    Returns:
+        spaces.Space: ゲーム情報を観測に変換したもの
+    """
     pass
