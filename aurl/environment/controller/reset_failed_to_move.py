8e374535b35793f0d57068856c1322ba6caba9c9
git-willow <y_vlookup8744@softbank.ne.jp>
2022-11-11 16:03:41 +0900

    リスト内の要素をすべてfalseにするように実装しました。

--- aurl/environment/controller/reset_failed_to_move.py
+++ aurl/environment/controller/reset_failed_to_move.py
@@ -2,12 +2,13 @@ from ..model import GameState
 
 
 def reset_failed_to_move(state: GameState) -> None:
-    """reset_failed_to_move
+    state.players = [i = false for i in state.players]
+
 
     各プレイヤーの壁衝突判定をリセットする
     state.playersの全ての要素に対して、以下を実行する
     1. failed_to_moveをFalseにする
-
+    
     Args:
         state (GameState): 現在のゲーム情報
     """
