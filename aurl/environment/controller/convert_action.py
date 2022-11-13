5d996a8079c0d4c1bdb7ca669a04c4c57ce125da
MoAI <64251336+MoAI522@users.noreply.github.com>
2022-11-13 15:25:07 +0900

    Feature/convert action (#22)
    * フォーマット設定
    
    * action_space修正
    
    * convert_action

--- aurl/environment/controller/convert_action.py
+++ aurl/environment/controller/convert_action.py
@@ -1,17 +1,23 @@
 from ..model import PlayerAction
-from gym.spaces import Space
 from typing import Dict
 
 
-def convert_action(action: Space) -> Dict[str, PlayerAction]:
+def convert_action(action) -> Dict[str, PlayerAction]:
     """convert_action
 
     step関数に入力されたactionはそのままだと扱いづらいので、こちらで定義したクラスのインスタンスに変換する
 
     Args:
-        action (Space): stepに入力されたaction
+        action (Any): stepに入力されたaction 型定義不可？
 
     Returns:
         Dict[str,PlayerAction]: 変換後のアクション情報、キーはプレイヤー番号
     """
-    pass
+    out = {}
+    for i, a in action.items():
+        sus = {}
+        for j, sus_value in enumerate(a[3]):
+            sus[str(j if j < int(i) else j + 1)] = sus_value
+        player_action = PlayerAction(a[0], a[1] == 1, a[2] == 1, sus)
+        out[i] = player_action
+    return out
