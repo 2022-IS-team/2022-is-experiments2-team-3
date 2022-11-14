b35692375649676dfda44b7fd2e18eb2bc07baff
MoAI522 <urotanre522@gmail.com>
2022-11-14 18:04:38 +0000

    update_sus

--- aurl/environment/controller/update_sus.py
+++ aurl/environment/controller/update_sus.py
@@ -1,5 +1,6 @@
 from ..model import GameState, PlayerAction
 from typing import Dict
+from .. import config
 
 
 def update_sus(action: Dict[str, PlayerAction], state: GameState) -> None:
@@ -14,4 +15,10 @@ def update_sus(action: Dict[str, PlayerAction], state: GameState) -> None:
         action (Dict[str,PlayerAction]): 各ユーザーの行動
         state (GameState): 現在のゲーム情報
     """
-    pass
+    for i, a in action.items():
+        p = state.players[int(i)]
+        for j in range(config.num_players):
+            if int(i) == j:
+                continue
+            idx = j if j < int(i) else j - 1
+            p.sus[str(j)] = a.sus[idx]
--- tests/environment/controller/test_update_sus.py
+++ tests/environment/controller/test_update_sus.py
@@ -9,11 +9,11 @@ def test_update_sus():
               "2":PlayerAction(0,False,False,[0.4,0.5,0.6,0.7]),
               "3":PlayerAction(0,False,False,[0.6,0.7,0.8,0.9]),
               "4":PlayerAction(0,False,False,[0.8,0.9,0.0,0.1])}
-    state = GameState(players=[PlayerState(0,(0,0)) for _ in range(5)], tasks=[TaskState((0,0),0) for _ in range(15)], game_map=np.ndarray((config.map_height,config.map_width)))
+    state = GameState(players=[PlayerState(0,(0,0),i) for i in range(5)], tasks=[TaskState((0,0),0) for _ in range(15)], game_map=np.ndarray((config.map_height,config.map_width)))
     update_sus(action=action,state=state)
-    assert state.players[0].sus == action["0"].sus
-    assert state.players[1].sus == action["1"].sus
-    assert state.players[2].sus == action["2"].sus
-    assert state.players[3].sus == action["3"].sus
-    assert state.players[4].sus == action["4"].sus
+    assert state.players[0].sus["2"] == action["0"].sus[1]
+    assert state.players[1].sus["3"] == action["1"].sus[2]
+    assert state.players[2].sus["4"] == action["2"].sus[3]
+    assert state.players[3].sus["0"] == action["3"].sus[0]
+    assert state.players[4].sus["1"] == action["4"].sus[1]
     
