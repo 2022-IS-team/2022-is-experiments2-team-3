9cb29205913a47ba55328ade2be025aaee020c4a
HarfmoonCat <wboiledxtreme2011@gmail.com>
2022-12-07 17:43:08 +0900

    通報可能かどうか

--- src/aurl/environment/controller/update_report_availability.py
+++ src/aurl/environment/controller/update_report_availability.py
@@ -16,12 +16,13 @@ def update_report_availability(state: GameState) -> None:
         state (GameState): 現在のゲーム情報
     """
     for i, p in enumerate(state.players):
-        p.report_available == False
+        p.report_available = False
         for j, q in enumerate(state.players):
             if i != j:
-                if q.role == 1:
+                if q.role == 0:
                     if q.dead == True:
                         if q.reported == False:
-                            if q.died_at == p.position:
-                                break
-                                p.report_available == True
+                            if p.position == q.died_at:
+                               p.report_available = True
+                               break
+
