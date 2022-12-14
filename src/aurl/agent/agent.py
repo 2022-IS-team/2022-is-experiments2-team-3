bdc423a71dff89e81eac68a05f4877c8473ae048
tsukisann24 <gsbr.chinocon13@gmail.com>
2022-12-14 14:55:33 +0900

    collect_rolloutの定義v1

--- src/aurl/agent/agent.py
+++ src/aurl/agent/agent.py
@@ -1,7 +1,11 @@
 from .vecenv import VecEnv
 from stable_baselines3.ppo.ppo import PPO
+from stable_baselines3.common.utils import obs_as_tensor, safe_mean
 from typing import List, Tuple
 import numpy as np
+
+import gym
+import torch as th
 
 
 class MultiAgentLearner:
@@ -25,7 +29,7 @@ class MultiAgentLearner:
         環境envをゲームが終わるまで回して、その間の各エージェントの行動とその結果環境から返された観測、報酬、価値(value)、log_probsを
         self._rollout_buffersにすべて格納する
         self._rollout_buffersは各エージェントのRolloutBufferのリスト
-        RolloutBufferはそれぞれ全ステップ分の(行動、1ステップ前の観測、報酬、価値、log_probs)のタプルのリスト
+        RolloutBufferはそれぞれ全ステップ分の(行動、1ステップ前の観測、報酬、_last_episode_starts、価値、log_probs)のタプルのリスト
 
         エージェントはそれぞれPPOのインスタンスとしてリストself._agentsの中に入っている
         行動の決定はself._agentsのそれぞれのpolicyで行う
@@ -36,6 +40,63 @@ class MultiAgentLearner:
         reward_listは報酬(float)のリスト
         doneはTrueならゲーム終了
         """
+        last_obs = None
+        self.policy.set_training_mode(False)
+
+        n_steps = 0
+        self._rollout_buffers = []
+
+        while n_steps < 2048:
+            actions_list = []
+            clipped_actions_list = []
+            values_list = []
+            log_probs_list = []
+
+            for i in range(5):
+                with th.no_grad():
+                    obs_tensor = obs_as_tensor(self.last_obs, self.device)
+                    actions, values, log_probs = obs_tensor
+                actions_list[i] = actions.cpu().numpy()
+                log_probs_list[i] = log_probs
+
+                clipped_actions = actions
+                if isinstance(self.action_space, gym.spaces.Box):
+                    clipped_actions_list[i] = np.clip(actions, self.action_space.low, self.action_space.high)
+
+            new_obs, rewards, dones, infos = env.step(actions_list)
+
+            self._update_info_buffer(infos)
+
+            n_steps += 1
+
+            # if isinstance(self.action_space, gym.space.Discrete):
+            #     actions = actions.reshape(-1,1)
+
+            #infos未定義?
+            for i in range(5):
+                for idx, done in enumerate(dones):
+                    if(
+                        done
+                        and infos[idx].get("terminal_observation") is not None
+                        and infos[idx].get("TimeLimit.truncated", False)
+                    ):
+                        terminal_obs = self.policy.obs_tensor(infos[idx]["terminal_observation"])[0]
+                        with th.no_grad():
+                            terminal_value = self.policy.predict_values(terminal_obs)[0]
+                        rewards[i][idx] += self.gamma * terminal_value
+            
+            #_last_episode_starts未実装
+            for i in range(5):
+                self._rollout_buffers[i].add(last_obs[i], actions_list[i], rewards[i], self._last_episode_starts, values[i], log_probs_list[i])
+                last_obs[i] = new_obs[i]
+
+            with th.no_grad():
+                for i in range(5):
+                    values[i] = self.policy.predict_values(obs_as_tensor(new_obs[i], self.device))
+
+            for i in range(5):
+                self._rollout_buffers.compute_returns_and_advantage(last_values=values[i], dones=dones)
+
         pass
 
     def _train(self):
